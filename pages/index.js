f805b862554f8bca288398c2e6ef24584ec0a27d
BuildTools <unconfigured@null.spigotmc.org>
2023-03-01 21:22:52 -0600

    sup

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
         
       </Head>
 
-      <main className='h-screen bg-transparent bg-white px-10 dark:bg-black flex justify-center items-center'>
+      <main className='h-screen bg-transparent bg-white px-10 dark:bg-black pt-16 flex justify-center items-center'>
         <section className='min-h-screen'>
          
          <nav className=" px-2 sm:px-4 py-2.5 rounded ">
@@ -75,14 +75,6 @@ export default function Home() {
       <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
     </button>
 
-    <div id="targetNav" class="hidden">
-  <ul class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-      <li class="w-full px-4 py-2 border-b border-gray-200 rounded-t-lg dark:border-gray-600">Profile</li>
-      <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">Settings</li>
-      <li class="w-full px-4 py-2 border-b border-gray-200 dark:border-gray-600">Messages</li>
-      <li class="w-full px-4 py-2 rounded-b-lg">Download</li>
-  </ul>
-</div>
 
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 ">
